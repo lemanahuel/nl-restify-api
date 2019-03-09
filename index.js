358524cff7d3a4692a990af1305dab6087754951
lemanahuel <lema.nahuel@gmail.com>
2019-03-09 07:56:06 -0300

    IMPROVE, archivos estaticos y render pug

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const config = require('./config/config');
 const restify = require('restify');
 const corsMiddleware = require('restify-cors-middleware');
+const render = require('restify-render-middleware');
 const glob = require('glob');
 const path = require('path');
 const _ = require('lodash');
@@ -24,26 +25,35 @@ server.use(restify.plugins.bodyParser({
 }));
 // server.use(restify.plugins.multipartBodyParser());
 // server.use(restify.plugins.requestExpiry());
-server.use(restify.plugins.throttle({
-  burst: 100,
-  rate: 50,
-  ip: true,
-  overrides: {
-    '192.168.1.1': {
-      rate: 0,
-      burst: 0
-    }
-  }
-}));
+// server.use(restify.plugins.throttle({
+//   burst: 100,
+//   rate: 50,
+//   ip: true,
+//   overrides: {
+//     '192.168.1.1': {
+//       rate: 0,
+//       burst: 0
+//     }
+//   }
+// }));
 server.use(restify.plugins.conditionalRequest());
 
 server.pre(cors.preflight);
 server.use(cors.actual);
 
+server.use(render({
+  engine: 'pug',
+  dir: __dirname
+}));
+
+//CUSTOM LOG
 server.use((req, res, next) => {
-  // req.log.info('TEST');
   console.log(req.method, req.url);
   next();
+});
+
+server.get('/', (req, res, next) => {
+  res.redirect(301, '/docs', next);
 });
 
 glob('./modules/**/*.routes.js', {}, (err, files) => {
@@ -51,6 +61,28 @@ glob('./modules/**/*.routes.js', {}, (err, files) => {
     require(path.resolve(file))(server);
     cb(null);
   }, err => {
+
+    // server.get('/docs', restify.plugins.serveStatic({
+    //   directory: './public',
+    //   default: 'index.html'
+    // }));
+    let routes = [];
+    _.mapValues(server.router._registry._routes, (value, key) => {
+      routes.push({
+        path: value.path,
+        method: value.method
+      });
+    });
+    server.get('/docs', (req, res, next) => {
+      res.render('./public/docs/docs.pug', {
+        app: {
+          title: 'Probando render de restify',
+          description: 'Endpoints:'
+        },
+        endpoints: routes
+      });
+    });
+
     server.listen(config.PORT, () => {
       console.log('%s listening at %s', server.name, server.url);
     });
